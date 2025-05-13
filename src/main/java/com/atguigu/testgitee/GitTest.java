56240284bb469d99ff9ad6ab11f3fed7651664d4
syc <c13803006169@163.com>
2025-05-13 15:07:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/testgitee/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.testgitee;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.testgitee
+ * Description:
+ *
+ * @Author
+ * @Create 2025/5/13 15:07
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
